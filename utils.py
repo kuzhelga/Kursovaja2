12c439865df043855dad7c1535afe9c73f612a4e
kuzhelga <kuzhelga81@gmail.com>
2023-04-02 15:45:33 +0300

    added function get_five_last_values

--- utils.py
+++ utils.py
@@ -14,6 +14,13 @@ def get_data():
 def get_filtered_data(data):
     """Функция фильтрует данные по статусу Исполнено (EXECUTED)
     """
-    data = [x for x in data if "state" in x and x["state"] == 'EXECUTED']
+    data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
     return data
+
+
+def get_five_last_values(data):
+    """Функция сортирует список по дате и возвращает последние 5 операций
+    """
+    data = sorted(data, key=lambda x: x['date'], reverse=True)
+    return data[:5]
 
