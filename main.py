5177670dfd922926dd44043970e6bb72ec546357
kuzhelga <kuzhelga81@gmail.com>
2023-04-02 19:32:55 +0300

    added print data

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from utils import get_data, get_filtered_data, get_five_last_values
+from utils import get_data, get_filtered_data, get_five_last_values, get_data_with_from, get_formated_data
 
 
 def main():
     data = get_data()
     data = get_filtered_data(data)
+    data = get_data_with_from(data)
     data = get_five_last_values(data)
+    data = get_formated_data(data)
 
-
+    for row in data:
+        print(row)
 
 
 if __name__ == '__main__':
     main()
-
