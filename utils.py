9e8b70fc8e4efba869061084973972706a31f676
kuzhelga <kuzhelga81@gmail.com>
2023-04-02 15:08:53 +0300

    added function to filter data with only executed state

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 import json
+from pprint import pprint
+
 
 def get_data():
     """Функция преобразует файл из формата json в python формат
     :return: data
     """
-    with open ('operations.json', 'r', encoding='utf-8') as file:
+    with open('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
+    return data
+
+
+def get_filtered_data(data):
+    """Функция фильтрует данные по статусу Исполнено (EXECUTED)
+    """
+    data = [x for x in data if "state" in x and x["state"] == 'EXECUTED']
     return data
 
