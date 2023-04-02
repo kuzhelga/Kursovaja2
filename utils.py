ceee9f65bf958c82f2070113d18467101b5f2888
kuzhelga <kuzhelga81@gmail.com>
2023-04-02 19:35:03 +0300

    cleaned accordingly PEP8

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 import json
 from datetime import datetime
-from pprint import pprint
 
 
 def get_data():
     """Функция преобразует файл из формата json в python формат
-    :return: data
     """
     with open('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
+
     return data
 
 
@@ -16,13 +15,15 @@ def get_filtered_data(data):
     """Функция фильтрует данные по статусу Исполнено (EXECUTED)
     """
     data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
+
     return data
 
 
 def get_data_with_from(data):
-    """Функция фильтрует данные и возвращет по наличию поля 'from'
+    """Функция фильтрует данные и возвращает по наличию поля 'from'
     """
     data = [x for x in data if 'from' in x]
+
     return data
 
 
@@ -30,6 +31,7 @@ def get_five_last_values(data):
     """Функция сортирует список по дате и возвращает последние 5 операций
     """
     data = sorted(data, key=lambda x: x['date'], reverse=True)
+
     return data[:5]
 
 
@@ -68,4 +70,5 @@ def get_formated_data(data):
         {date} {description}
         {from_info} {from_source} -> {to_info} {to_destination}
         {row['operationAmount']['amount']} {row['operationAmount']['currency']['name']}""")
+
     return formated_data
