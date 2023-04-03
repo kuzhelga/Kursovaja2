8c90b0ebb639296239211087d97f383c25826373
kuzhelga <kuzhelga81@gmail.com>
2023-04-03 21:01:52 +0300

    added test_get_five_last_values

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from utils import get_data, get_filtered_data, get_data_with_from
+from utils import get_data, get_filtered_data, get_data_with_from, get_five_last_values
 
 
 def test_get_data():
@@ -15,4 +15,7 @@ def test_get_filtered_data(test_data):
 def test_get_data_with_from(test_data):
    assert len(get_data_with_from(test_data)) == 5
 
+
+def test_get_five_last_values(test_data):
+    assert len(get_five_last_values(test_data)) == 5
 
