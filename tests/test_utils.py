b9932145abb61c605eab1f59276818ee7ea7f7d8
kuzhelga <kuzhelga81@gmail.com>
2023-04-03 20:57:37 +0300

    added test_get_data_with_from

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from utils import get_data, get_filtered_data
+from utils import get_data, get_filtered_data, get_data_with_from
 
 
 def test_get_data():
@@ -9,4 +9,10 @@ def test_get_data():
 
 
 def test_get_filtered_data(test_data):
-    assert len(get_filtered_data(test_data)) == 1
+    assert len(get_filtered_data(test_data)) == 6
+
+
+def test_get_data_with_from(test_data):
+   assert len(get_data_with_from(test_data)) == 5
+
+
