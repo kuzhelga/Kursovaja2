b454e431cc0a803d8f3b4f37d4586c14f7c0d5c6
kuzhelga <kuzhelga81@gmail.com>
2023-04-04 20:50:38 +0300

    deleted function to filter records without 'from'

--- utils.py
+++ utils.py
@@ -19,14 +19,6 @@ def get_filtered_data(data):
     return data
 
 
-def get_data_with_from(data):
-    """Функция фильтрует данные и возвращает по наличию поля 'from'
-    """
-    data = [x for x in data if 'from' in x]
-
-    return data
-
-
 def get_five_last_values(data):
     """Функция сортирует список по дате и возвращает последние 5 операций
     """
@@ -44,16 +36,18 @@ def get_formated_data(data):
 
         description = row['description']
 
-        sender = row['from'].split()
-        from_source = sender.pop(-1)
-        if len(from_source) == 16:
-            from_source = f'{from_source[:4]} {from_source[4:6]}** **** {from_source[-4:]}'
-            from_info = ' '.join(sender)
-        elif len(from_source) == 20:
-            from_source = f'** {from_source[-4:]}'
-            from_info = ' '.join(sender)
+        if "from" in row:
+            sender = row['from'].split()
+            from_source = sender.pop(-1)
+            if len(from_source) == 16:
+                from_source = f'{from_source[:4]} {from_source[4:6]}** **** {from_source[-4:]}'
+                from_info = ' '.join(sender)
+            elif len(from_source) == 20:
+                from_source = f'** {from_source[-4:]}'
+                from_info = ' '.join(sender)
         else:
-            print('this is not a card or account')
+            from_info, from_source = "Отправитель скрыт", ""
+
 
         recipient = row['to'].split()
         to_destination = recipient.pop(-1)
