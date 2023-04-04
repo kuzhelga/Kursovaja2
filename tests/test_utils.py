087c9eca1e25f2edc56dd5963a692bde05c24d01
kuzhelga <kuzhelga81@gmail.com>
2023-04-04 20:58:08 +0300

    deleted test for deleted function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from utils import get_data, get_filtered_data, get_data_with_from, get_five_last_values, get_formated_data
+from utils import get_data, get_filtered_data, get_five_last_values, get_formated_data
 
 
 def test_get_data():
@@ -12,14 +12,9 @@ def test_get_filtered_data(test_data):
     assert len(get_filtered_data(test_data)) == 6
 
 
-def test_get_data_with_from(test_data):
-   assert len(get_data_with_from(test_data)) == 5
-
-
 def test_get_five_last_values(test_data):
     assert len(get_five_last_values(test_data)) == 5
 
 
 def test_get_formated_data(test_data):
-    data = get_data_with_from(test_data)
-    assert type(get_formated_data(data)) == list
+    assert type(get_formated_data(test_data)) == list
